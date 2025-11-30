ad89b27f632ac7bba955e169448df5815eaf0a88
Gonzalo Portilla <gonzalo.portilla@opendeusto.es>
2025-11-30 16:06:30 +0100

    Falta modificar el Service (mirar TCPSocket)

---
+++ ContSocket/src/test/java/server/socket/ContSocket/ContSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package server.socket.ContSocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContSocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
