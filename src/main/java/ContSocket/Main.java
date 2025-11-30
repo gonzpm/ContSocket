7fb880cc88fe0f332cc3a0d741a8c5213bc9e821
Naia Martin <naia.martin@opendeusto.es>
2025-11-30 23:35:36 +0100

    m

--- src/main/java/ContSocket/Main.java
+++ src/main/java/ContSocket/Main.java
@@ -3,8 +3,7 @@ package ContSocket;
 public class Main {
     public static void main(String[] args) {
 
-        int port = 8082; // choose your port
-
+        int port = 8082; 
         System.out.println("Starting ContSocket server on port " + port + "...");
         TCPContSocketServer server = new TCPContSocketServer(port);
         server.start();
